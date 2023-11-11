742880b0e1d844d69d769e0ba6d29207b04703af
TianKai Ma <tiankaima@163.com>
2023-11-11 13:29:52 +0800

    feat: update typst-library adapter

--- crates/typst-cli/src/lib.rs
+++ crates/typst-cli/src/lib.rs
@@ -1,23 +1,26 @@
 mod args;
 mod compile;
+mod download;
 mod fonts;
 mod package;
+mod query;
+#[cfg(feature = "self-update")]
 mod tracing;
 mod watch;
 mod world;
 
-use clap::Parser;
-// use std::cell::Cell;
-// use std::process::ExitCode;
+use std::cell::Cell;
 use std::io::{self, IsTerminal, Write};
+
+use clap::Parser;
 use codespan_reporting::term::{self, termcolor};
+use once_cell::sync::Lazy;
 use termcolor::{ColorChoice, WriteColor};
 use crate::args::{CliArguments, Command};
 
-// thread_local! {
-    /// The CLI's exit code.
-    // static EXIT: Cell<ExitCode> = Cell::new(ExitCode::SUCCESS);
-// }
+
+/// The parsed commandline arguments.
+static ARGS: Lazy<CliArguments> = Lazy::new(CliArguments::parse);
 
 #[no_mangle]
 pub extern fn run(command: *const u8) -> i32 {
@@ -25,22 +28,16 @@ pub extern fn run(command: *const u8) -> i32 {
     let command = unsafe { std::ffi::CStr::from_ptr(command as *const i8) };
     let command = command.to_str().unwrap();
     let arguments = CliArguments::parse_from(command.split(" ").collect::<Vec<&str>>());
-    // let _guard = match crate::tracing::setup_tracing(&arguments) {
-    //     Ok(guard) => guard,
-    //     Err(err) => {
-    //         eprintln!("failed to initialize tracing {}", err);
-    //         None
-    //     }
-    // };
 
     let res = match arguments.command {
-        Command::Compile(command) => crate::compile::compile(command),
-        Command::Watch(command) => crate::watch::watch(command),
-        Command::Fonts(command) => crate::fonts::fonts(command),
+        Command::Compile(command) => crate::compile::compile(command.clone()),
+        Command::Watch(command) => crate::watch::watch(command.clone()),
+        Command::Query(command) => crate::query::query(&command),
+        Command::Fonts(command) => crate::fonts::fonts(&command),
+        Command::Update(command) => crate::update::update(&command),
     };
 
     if let Err(msg) = res {
-        set_failed();
         print_error(&msg).expect("failed to print error");
     }
 
@@ -48,6 +45,7 @@ pub extern fn run(command: *const u8) -> i32 {
     0
 }
 
+/// Ensure a failure exit code.
 fn set_failed() {
     // EXIT.with(|cell| cell.set(ExitCode::FAILURE));
 }
@@ -76,4 +74,18 @@ fn color_stream() -> termcolor::StandardStream {
 /// Used by `args.rs`.
 fn typst_version() -> &'static str {
     env!("TYPST_VERSION")
+}
+
+#[cfg(not(feature = "self-update"))]
+mod update {
+    use crate::args::UpdateCommand;
+    use typst::diag::{bail, StrResult};
+
+    pub fn update(_: &UpdateCommand) -> StrResult<()> {
+        bail!(
+            "self-updating is not enabled for this executable, \
+             please update with the package manager or mechanism \
+             used for initial installation"
+        )
+    }
 }
